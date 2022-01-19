7379cb9192974693afc4e998669129515fbfa6ea
williamcamasil <williamcamargosilva@gmail.com>
2022-01-18 21:41:19 -0300

    created a service with observable to obtain values from api async

--- lh-angular/src/app/vagas.service.ts
+++ lh-angular/src/app/vagas.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VagasService {
 
-  constructor() { }
+  private url = "";
+
+  constructor(private _httpClient: HttpClient) {
+
+  }
+
+  //Observable: responsável por fazer o retorno assincrono
+  getVagas(): Observable<Vaga[]> {
+    return this._httpClient.get<Vaga[]>(this.url);
+  }
 }
